c66e23871b392959a330171cd18837fe6bbec0d2
hvalmer <hvalmer@gmail.com>
2026-09-17 16:10:13 -0300

    feat: implement JPA repositories

---
+++ src/main/java/com/valmerdev/deliveryapi/infrastructure/repository/CidadeRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.valmerdev.deliveryapi.infrastructure.repository;
+
+import com.valmerdev.deliveryapi.domain.model.Cidade;
+import com.valmerdev.deliveryapi.domain.repository.CidadeRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class CidadeRepositoryImpl implements CidadeRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Cidade> listar() {
+        return manager.createQuery("from Cidade", Cidade.class)
+                .getResultList();
+    }
+
+    @Override
+    public Cidade buscar(Long id) {
+        return manager.find(Cidade.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Cidade salvar(Cidade cidade) {
+        return manager.merge(cidade);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cidade cidade) {
+        cidade = buscar(cidade.getId());
+        manager.remove(cidade);
+
+    }
+}
---
+++ src/main/java/com/valmerdev/deliveryapi/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.valmerdev.deliveryapi.infrastructure.repository;
+
+import com.valmerdev.deliveryapi.domain.model.Cozinha;
+import com.valmerdev.deliveryapi.domain.repository.CozinhaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Cozinha> todas(){
+        return manager.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
+    }
+
+    @Override
+    public Cozinha porId(Long id){
+        return manager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Cozinha adicionar(Cozinha cozinha){
+        return manager.merge(cozinha);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cozinha cozinha){
+        cozinha = porId(cozinha.getId());
+        manager.remove(cozinha);
+    }
+}
